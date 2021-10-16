7617faf4d97a842a84a1dbbda1a21c7c70f94d1c
andreslc47 <andreslc47@hotmail.com>
2021-10-16 13:47:25 -0500

    cleaned everything, only zip file ready for download

--- build/add_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Project5_udacity/src/add_markers
-# Build directory: /home/robond/Project5_udacity/build/add_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot_interactions/turtlebot_interactions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Project5_udacity/src/turtlebot_interactions/turtlebot_interactions
-# Build directory: /home/robond/Project5_udacity/build/turtlebot_interactions/turtlebot_interactions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
